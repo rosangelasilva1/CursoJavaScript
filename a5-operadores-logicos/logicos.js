7e7cc86d0669b730ad57f1963fa512814f540857
rosangelasilva1 <roliveirasv@gmail.com>
2020-02-24 15:26:02 -0300

    Operadores lógicos

---
+++ a5-operadores-logicos/logicos.js
@@ -0,0 +1,17 @@
+function oplogicos(){
+
+    a=1;
+    b=2;
+    c=3;
+    document.write(a);
+    document.write(b);
+    document.write(c);
+    if(a==b && a==c){
+        alert("Exemplo de Operador Lógico  E (&&)");
+        alert("a =b e a=c");
+    }else{
+        alert("Os numeros sao diferentes");
+    }
+  
+
+}
